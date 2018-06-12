nzioki/firebird

Initial implementation of time zone support - CORE-694.
TODO:
- Support regions.
- Reevaluate decision of store date/time components in memory in its own time zone instead of normalized to UTC.
- Fix ISQL's SET TIME command.
- Allow usage of "WITHOUT TIME ZONE" with TIME/TIMESTAMP data types.
- Think about compatibility of CURRENT_TIME/CURRENT_TIMEZONE.
- Support AT { LOCAL | TIME ZONE <zone> } expression.
- Support as needed and fix assertions when altering field types.
- Change conversion to string to put a space before the time zone.
- Big endian version of SKD_time_tz and SKD_timestamp_tz.

src/jrd/opt.cpp
@@ -397,7 +397,9 @@ static const UCHAR sort_dtypes[] =
 	SKD_bytes,					// dtype_boolean
 	SKD_dec64,					// dtype_dec64
 	SKD_dec128,					// dtype_dec128
-	SKD_dec_fixed				// dtype_dec_fixed
+	SKD_dec_fixed,				// dtype_dec_fixed
+	SKD_sql_time_tz,			// dtype_sql_time_tz
+	SKD_timestamp_tz			// dtype_timestamp_tz
 };
 
 
@@ -2459,7 +2461,8 @@ SortedStream* OPT_gen_sort(thread_db* tdbb, CompilerScratch* csb, const StreamLi
 
 						// International type text has a computed key
 						// Different decimal float values sometimes have same keys
-						if (IS_INTL_DATA(desc) || desc->isDecFloat())
+						// ASF: Date/time with time zones too.
+						if (IS_INTL_DATA(desc) || desc->isDecFloat() || desc->isDateTimeTz())
 							break;
 
 						--items;

tadryanom/FirebirdSQL_firebird

src/jrd/opt.cpp
@@ -397,7 +397,9 @@ static const UCHAR sort_dtypes[] =
 	SKD_bytes,					// dtype_boolean
 	SKD_dec64,					// dtype_dec64
 	SKD_dec128,					// dtype_dec128
-	SKD_dec128					// dtype_dec_fixed
+	SKD_dec128,					// dtype_dec_fixed
+	SKD_sql_time_tz,			// dtype_sql_time_tz
+	SKD_timestamp_tz			// dtype_timestamp_tz
 };
 
 
@@ -2459,7 +2461,8 @@ SortedStream* OPT_gen_sort(thread_db* tdbb, CompilerScratch* csb, const StreamLi
 
 						// International type text has a computed key
 						// Different decimal float values sometimes have same keys
-						if (IS_INTL_DATA(desc) || desc->isDecFloat())
+						// ASF: Date/time with time zones too.
+						if (IS_INTL_DATA(desc) || desc->isDecFloat() || desc->isDateTimeTz())
 							break;
 
 						--items;