799a0a6823e89f9468bfa9006c03aab6c773ea02
AlexPeshkoff <alexander.peshkoff@gmail.com>
2023-01-12 20:31:24 +0300

    Code cleanup related with #7446: Attempt to use data in destroyed transaction pool

--- src/jrd/tra.cpp
+++ src/jrd/tra.cpp
@@ -240,8 +240,9 @@ void TRA_detach_request(Jrd::Request* request)
 	fb_assert(!request->req_savepoints);
 
 	// Release procedure savepoints used by this request
-	if (request->req_proc_sav_point && (request->req_flags & req_proc_fetch))
+	if (request->req_proc_sav_point)
 	{
+		fb_assert(request->req_flags & req_proc_fetch);
 		Savepoint::destroy(request->req_proc_sav_point);
 		fb_assert(!request->req_proc_sav_point);
 	}
